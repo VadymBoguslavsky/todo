ce76f0698c75a92e99123b96dc56315c7a7ecdc5
vadym <axixe92@gmail.com>
2017-01-10 16:07:30 +0200

    update smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,10 +55,10 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "todo_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+
  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'peaceful-spire-21565.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      "axixe92@gmail.com",
@@ -66,10 +66,10 @@ Rails.application.configure do
     domain:         "gmail.com",
     address:       "smtp.gmail.com",
     port:          587,
-    authentication: :login,
+    authentication: "plain",
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
