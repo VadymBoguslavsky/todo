033657b29c5384af5d488b43362641e07c010a85
vadym <axixe92@gmail.com>
2017-01-25 21:34:07 +0200

    PriorityInput

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,4 @@
 class Task < ApplicationRecord
-  PRIORITIES = [
-      ['Later', 1],
-      ['Next', 2],
-      ['Now', 3]
-  ]
-
   validates :title, presence: true, length: {minimum: 5, too_short: "%{count} characters is the minimum allowed"}
-
   belongs_to :user
-
 end
