ec3c494307227da297a0743bfcd27786b0b3ca2f
vadym <axixe92@gmail.com>
2017-01-30 13:15:50 +0200

    Nav. Its not scrabble anymore

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5, too_short: "%{count} characters is the minimum allowed"}
+  validates :priority, presence: true
   belongs_to :user
 end
