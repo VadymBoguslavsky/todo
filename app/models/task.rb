a7a34d210b0d34d8ed87022a8342473c8bf8f0bf
vadym <axixe92@gmail.com>
2017-02-02 11:47:40 +0200

    Integer priority

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5, too_short: "%{count} characters is the minimum allowed"}
-  validates :priority, presence: true
+  validates :priority, presence: true, numericality: { only_integer: true }
   belongs_to :user
 end
