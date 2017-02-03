77fa23e02bb37d9e2e08d265d6e659f8aa1f1680
vadym <axixe92@gmail.com>
2017-02-03 16:37:58 +0200

    buttons Checkout and checkall

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,32 +38,60 @@
  Licenced as X11: http://www.kryogenix.org/code/browser/licence.html
  This basically means: do what you want with it.
  */
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery.purr
+//= require best_in_place
+//= require turbolinks
+//= require_tree .
 
 
+function check() {
+    var check = $('#form1').find('input');
+    for (var i = 0; i < check.length; i++) {
+        if (check[i].type == 'checkbox') {
+            if (('#form1 input:checkbox:checked').length > 0) {
+                $("#but_disabled1").removeAttr("disabled")
+            }
+            check[i].checked = true;
 
-
-$(document).on("turbolinks:load", function(){
-
-    $('#form1 input:checkbox').change(function () {
-              if ($('#form1 input:checkbox:checked').length>0) {
-            $("#but_disabled1").removeAttr( "disabled")  ;
-        } else {
-            $("#but_disabled1").attr( "disabled", "disabled")  ;
         }
-    });
-
-    $('#form2 input:checkbox').change(function () {
-        if ($('#form2 input:checkbox:checked').length>0) {
-            $("#but_disabled2").removeAttr( "disabled")  ;
-        } else {
-            $("#but_disabled2").attr( "disabled", "disabled")  ;
+    }
+}
+function uncheck() {
+    var uncheck = $('#form1').find('input');
+    for (var i = 0; i < uncheck.length; i++) {
+        if (uncheck[i].type == 'checkbox') {
+            $("#but_disabled1").attr("disabled", "disabled");
+            uncheck[i].checked = false;
         }
-    });
-});
+    }
+}
 
+function check1() {
+    var check = $('#form2').find('input');
+    for (var i = 0; i < check.length; i++) {
+        if (check[i].type == 'checkbox') {
+            if (('#form2 input:checkbox:checked').length > 0) {
+                $("#but_disabled2").removeAttr("disabled")
+            }
+            check[i].checked = true;
+        }
+    }
+}
+function uncheck1() {
+    var uncheck = $('#form2').find('input');
+    for (var i = 0; i < uncheck.length; i++) {
+        if (uncheck[i].type == 'checkbox') {
+            $("#but_disabled2").attr("disabled", "disabled");
+            uncheck[i].checked = false;
+        }
+    }
+}
 
-$(document).ready(function() {
-       jQuery(".best_in_place").best_in_place();
+$(document).ready(function () {
+    jQuery(".best_in_place").best_in_place();
 });
 
 var stIsIE = /*@cc_on!@*/false;
