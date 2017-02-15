2361dca7184a800339ba10d6b49a1d029a7320c4
VadymBoguslavsky <axixe92@gmail.com>
2017-02-15 11:47:56 +0200

    manually closed flash and active l

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,25 @@ module ApplicationHelper
   def bootstrap_class_for(flash_type)
     case flash_type
       when "success"
-        "alert-success"   # Green
+        "alert-success" # Green
       when "error"
-        "alert-danger"    # Red
+        "alert-danger" # Red
       when "alert"
-        "alert-warning"   # Yellow
+        "alert-warning" # Yellow
       when "notice"
-        "alert-info"      # Blue
+        "alert-info" # Blue
       else
         flash_type.to_s
     end
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+        concat content_tag(:button, 'x', class: "close", data: {dismiss: 'alert'})
+        concat message
+      end)
+    end
+    nil
   end
 end
